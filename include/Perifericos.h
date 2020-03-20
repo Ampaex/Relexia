e304117eed55a39deb36656865af829dd8686c0c
Antonio Vazquez <antonio@AntonioPC.home.com>
2020-03-20 23:22:30 +0100

    Project images

---
+++ include/Perifericos.h
@@ -0,0 +1,20 @@
+//          CABECERA LIBRERÍA DE CONTROL DE PERIFÉRICOS RELEXIA
+//                Autor: Antonio Vázquez Pérez
+//    Fecha de creación: 20/03/2020
+//       Última versión: 20/03/2020
+
+
+#include <WiFi.h>
+
+
+
+#define DEBUG 1
+
+//WiFi
+#define SSID "MOVISTAR_245E"
+#define CONTRASENA "gvp2x6HcgFv5J4oDXgkd"
+
+
+//WiFi
+void conectaWiFi();
+int compruebaWiFi();
