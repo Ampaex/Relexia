5fd47fd5e1e399819ac9ab446ff54f6751d2c8ad
Antonio Vazquez <antonio@AntonioPC.home.com>
2020-03-20 21:11:56 +0100

    Primer commit, inicialización del proyecto en visual studio code

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//          CÓDIGO PRINCIPAL DISPOSITIVO RELEXA
+//                Autor: Antonio Vázquez Pérez
+//    Fecha de creación: 20/03/2020
+//       Última versión: 20/03/2020
+
+#include <Arduino.h>
+
+
+void setup() {
+    Serial.begin(115200);
+}
+
+void loop() {
+
+}
